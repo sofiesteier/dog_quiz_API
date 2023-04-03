bab460bf6e180ac5a1c7f62c48658045a4997d4e
sofiesteier <steiersofie@gmail.com>
2023-04-03 16:00:25 +0200

    Added a error message
    You get a error message if a username already exists

--- api/register.php
+++ api/register.php
@@ -5,17 +5,24 @@ require_once("functions.php");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-$contentType = $_SERVER["CONTENT_TYPE"];
+$allowedMethods = ["POST"];
 
-$userFileJSON = "data.json";
+if(!in_array($requestMethod, $allowedMethods)) {
+    $error = ["error" => "Invalid HTTP method."];
+    sendJSON($error, 405);
+}
+
+$contentType = $_SERVER["CONTENT_TYPE"];
 
 if($contentType != "application/json") {
     $error = ["error" => "Invalid Content Type. Only JSON is allowed."];
-    sendJSON($error, 405);
+    sendJSON($error, 400);
 }
 
 $requestJSON = file_get_contents("php://input");
 $requestData = json_decode($requestJSON, true);
+
+$userFileJSON = "data.json";
 
 $users = [];
 
@@ -24,23 +31,29 @@ if(file_exists($userFileJSON)) {
     $users = json_decode($json, true);
 } 
 
-if($requestMethod == "POST") {
-    if(empty($requestData["username"]) || empty($requestData["password"])) {
-        $error = ["message" => "Bad Request (empty values)"];
-        sendJSON($error, 400);
-    }
-
-    $username = $requestData["username"];
-    $password = $requestData["password"];   
-    
-    $newUser = ["username" => $username, "password" => $password, "points" => 0];
-    $users[] = $newUser;
-
-    $json = json_encode($users, JSON_PRETTY_PRINT);
-
-    file_put_contents($userFileJSON, $json);
-    sendJSON($newUser);
+if(empty($requestData["username"]) || empty($requestData["password"])) {
+    $error = ["message" => "Bad Request (empty values)"];
+    sendJSON($error, 400);
 }
+
+$username = $requestData["username"];
+$password = $requestData["password"];   
+
+foreach($users as $user) {
+    if($user["username"] == $username) {
+        $error = ["message" => "Conflict (the username is already taken)"];
+        sendJSON($error, 409);
+    }
+}
+
+$newUser = ["username" => $username, "password" => $password, "points" => 0];
+$users[] = $newUser;
+$json = json_encode($users, JSON_PRETTY_PRINT);
+file_put_contents($userFileJSON, $json);
+sendJSON($newUser, 201);
+
+
+
 
 
 
