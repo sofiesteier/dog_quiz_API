18860b03957deb48144b87df23e4f8ed3f4e5b97
sofiesteier <steiersofie@gmail.com>
2023-04-04 13:26:41 +0200

    Added the file quiz.php

---
+++ api/login.php
@@ -0,0 +1,49 @@
+<?php
+ini_set("display_errors", 1);
+
+require_once("functions.php");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$allowedMethods = ["POST"];
+
+if(!in_array($requestMethod, $allowedMethods)) {
+    $error = ["error" => "Invalid HTTP method."];
+    sendJSON($error, 405);
+}
+
+$contentType = $_SERVER["CONTENT_TYPE"];
+
+if($contentType != "application/json") {
+    $error = ["error" => "Invalid Content Type. Only JSON is allowed."];
+    sendJSON($error, 400);
+}
+
+$requestJSON = file_get_contents("php://input");
+$requestData = json_decode($requestJSON, true);
+
+$userFileJSON = "data.json";
+
+$users = [];
+
+if(file_exists($userFileJSON)) {
+    $json = file_get_contents($userFileJSON);
+    $users = json_decode($json, true);
+} else {
+    $error = ["error" => "JSON file does not exists."]; 
+    sendJSON($error);
+}
+
+$username = $requestData["username"];
+$password = $requestData["password"];
+
+foreach($users as $user) {
+    if($user["username"] == $username and $user["password"] == $password){
+        sendJSON($user);
+    }
+
+    $error = ["message" => "Not found"];
+    sendJSON($error);
+}
+
+?>
---
+++ api/quiz.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ini_set("display_errors", 1);
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$allowedMethods = ["GET"];
+
+if($requestMethod != $allowedMethods) {
+
+}
+
+$userFileJSON = "data.json";
+
+$users = [];
+
+if(file_exists($userFileJSON)) {
+    $json = file_get_contents($userFileJSON);
+    $users = json_decode($json, true);
+} 
+*/
+
+
+?>
