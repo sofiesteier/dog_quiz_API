50b3e54f12b5e755dffab2d4863b83e844232630
sofiesteier <steiersofie@gmail.com>
2023-04-06 20:53:02 +0200

    Added code to get a picture and alternatives to the quiz

--- api/quiz.php
+++ api/quiz.php
@@ -1,13 +1,15 @@
 <?php
 
 ini_set("display_errors", 1);
-/*
+
+require_once("functions.php");
+
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
 $allowedMethods = ["GET"];
 
-if($requestMethod != $allowedMethods) {
-    $error = ["error" => "Invalid HTTP method."];
+if(!in_array($requestMethod, $allowedMethods)) {
+    $error = ["message" => "Invalid HTTP method."];
     sendJSON($error, 405);
 }
 
@@ -18,9 +20,10 @@ $users = [];
 if(file_exists($userFileJSON)) {
     $json = file_get_contents($userFileJSON);
     $users = json_decode($json, true);
-} 
-*/
-
+} else {
+    $error = ["message" => "JSON file does not exists."]; 
+    sendJSON($error);
+}
 
 $images = scandir("../images", $sorting_order = SCANDIR_SORT_ASCENDING);
 
@@ -46,35 +49,38 @@ $dogsJSON = "dogs.json";
 if(!file_exists($dogsJSON)) {
     $json = json_encode($dogs, JSON_PRETTY_PRINT);
     file_put_contents($dogsJSON, $json);
-}
+} 
 
+shuffle($dogs);
 
+$four_dogs = array_splice($dogs, 0, 4);
 
-echo "<pre>";
-var_dump($dogs);
-echo "<pre/>";
-
-
-shuffle($array_of_images);
-$four_dogs = array_splice($array_of_images, 2, 4);
-$random_index = rand(0, count($four_dogs));
+$random_index = rand(0, 3);
 
 $correct_dog = $four_dogs[$random_index];
 
-echo "<pre>";
-var_dump($correct_dog);
-echo "<pre/>";
+foreach($four_dogs as $dog) {
+    
+    $quizAnswer[] = [
+        "correct" => ($dog == $correct_dog ? true : false),
+        "name" => $dog["name"],
+    ];
 
+}
 
+$quizInfo = [
+    "image" => "images/" . $dog["url"],
+    "alternatives" => $quizAnswer,
+];
 
-
-
-
-
-
+header("Content-Type: application/json");
+http_response_code(200);
+echo json_encode($quizInfo);
+exit();
 
 /*
 header("Content-Type: application/json");
 echo '{"image":"images\/German_Shepherd.jpg","alternatives":[{"correct":false,"name":"Shetland Sheepdog"},{"correct":false,"name":"Jack Russell Terrier"},{"correct":true,"name":"German Shepherd"},{"correct":false,"name":"Labrador Retriever"}]}'
+exit();
 */
 ?>
