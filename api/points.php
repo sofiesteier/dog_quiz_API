feb4d3085d798d3a1003bf3ef9c72b51a2a8493c
sofiesteier <steiersofie@gmail.com>
2023-04-08 20:44:46 +0200

    Added code for the highscore

--- api/points.php
+++ api/points.php
@@ -1,68 +1,109 @@
 <?php
 ini_set("display_errors", 1);
 
-
 require_once("functions.php");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-$allowedMethods = ["POST"];
+$allowedMethods = ["POST", "GET"];
 
 if(!in_array($requestMethod, $allowedMethods)) {
     $error = ["error" => "Invalid HTTP method."];
     sendJSON($error, 405);
 }
 
-$contentType = $_SERVER["CONTENT_TYPE"];
+if($requestMethod == "POST") {
 
-if($contentType != "application/json") {
-    $error = ["error" => "Invalid Content Type. Only JSON is allowed."];
+    $contentType = $_SERVER["CONTENT_TYPE"];
+
+    if($contentType != "application/json") {
+    $error = ["message" => "Invalid Content Type. Only JSON is allowed."];
     sendJSON($error, 400);
-}
-
-$requestJSON = file_get_contents("php://input");
-$requestData = json_decode($requestJSON, true);
-
-$userFileJSON = "data.json";
-
-if(file_exists($userFileJSON)) {
-    $json = file_get_contents($userFileJSON);
-    $users = json_decode($json, true);
-} else {
-    $error = ["error" => "JSON file does not exists."]; 
-    sendJSON($error, 404);
-}
-
-$username = $requestData["username"];
-$password = $requestData["password"];
-$UserPoints = $requestData["points"];
-
-$updatedUsers = [];
-
-foreach($users as $user) {
-    
-    $userInfo = [
-        "username" => $user["username"],
-        "password" => $user["password"],
-        "points" => ($user["username"] == $username ? $user["points"] + $UserPoints : $user["points"]),
-    ];
-
-    $updatedUsers[] = $userInfo;
-
-}
-
-$json = json_encode($updatedUsers, JSON_PRETTY_PRINT);
-file_put_contents($userFileJSON, $json);
-
-foreach($users as $user) {
-    if($user["username"] == $username) {
-        $newPoints = ["points" => $user["points"] + $UserPoints];
     }
+
+    $requestJSON = file_get_contents("php://input");
+    $requestData = json_decode($requestJSON, true);
+
+
+    $username = $requestData["username"];
+    $password = $requestData["password"];
+    $UserPoints = $requestData["points"];
+
+
+    $requestJSON = file_get_contents("php://input");
+    $requestData = json_decode($requestJSON, true);
+
+    $userFileJSON = "data.json";
+
+    if(file_exists($userFileJSON)) {
+        $json = file_get_contents($userFileJSON);
+        $users = json_decode($json, true);
+    } else {
+        $error = ["error" => "JSON file does not exists."]; 
+        sendJSON($error, 404);
+    }
+
+    $updatedUsers = [];
+
+    foreach($users as $user) {
+
+        $userInfo = [
+            "username" => $user["username"],
+            "password" => $user["password"],
+            "points" => ($user["username"] == $username ? $user["points"] + $UserPoints : $user["points"]),
+        ];
+
+        $updatedUsers[] = $userInfo;
+
+    }
+
+    $json = json_encode($updatedUsers, JSON_PRETTY_PRINT);
+    file_put_contents($userFileJSON, $json);
+
+    foreach($users as $user) {
+        if($user["username"] == $username) {
+            $newPoints = ["points" => $user["points"] + $UserPoints];
+        }
+    }
+
+    sendJSON($newPoints);
 }
 
-header("Content-Type: application/json");
-http_response_code(200);
-echo json_encode($newPoints);
-exit();
+if($requestMethod == "GET") {
+
+    $userFileJSON = "data.json";
+    if(file_exists($userFileJSON)) {
+        $json = file_get_contents($userFileJSON);
+        $users = json_decode($json, true);
+    } else {
+        $error = ["error" => "JSON file does not exists."]; 
+        sendJSON($error, 404);
+    }
+    
+    usort($users, function ($value1, $value2) {
+        if ($value1["points"] == $value2["points"]) {
+            return 0;
+        }
+        return ($value1["points"] < $value2["points"]) ? 1 : -1;
+    });
+    
+    $firstFiveUsers = array_splice($users, 0, 5);
+    $topFiveHighscore = [];
+    foreach($firstFiveUsers as $user) {
+        $userTopFive = $user["username"];
+        $pointsTopFive = $user["points"];
+        $userHighscore = [
+            "username" => $userTopFive,
+            "points" => $pointsTopFive,
+        ];
+    
+        $topFiveHighscore[] = $userHighscore;
+    }
+
+    sendJSON($topFiveHighscore);
+
+}
+
+
 
 ?>
